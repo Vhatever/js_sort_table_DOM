c0287163f04a811c5148bb3607a9a93ee64352d4
Vhatever <fserrorka@gmail.com>
2025-04-16 15:08:18 +0300

    tryN1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+
+table.querySelectorAll('th').forEach((th, columnIndex) => {
+  th.addEventListener('click', () => {
+    const rows = Array.from(table.querySelector('tbody').rows);
+
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.cells[columnIndex].textContent.trim();
+      const cellB = rowB.cells[columnIndex].textContent.trim();
+
+      const a = isNaN(cellA) ? cellA : Number(cellA);
+      const b = isNaN(cellB) ? cellB : Number(cellB);
+
+      return a > b ? 1 : -1;
+    });
+
+    const tbody = table.querySelector('tbody');
+
+    tbody.innerHTML = '';
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
